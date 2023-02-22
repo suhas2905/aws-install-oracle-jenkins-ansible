38fa1fd7560fc9d1028a41ff6330c5ed57a6821f
Cloud User <ec2-user@ip-10-200-128-11.ec2.internal>
2023-02-22 10:55:09 +0000

    added

--- jenkins-pipelines/sap-pipeline-destroy-oracle-pas-ascs/stages/destroy_env/run_terraform.sh
+++ jenkins-pipelines/sap-pipeline-destroy-oracle-pas-ascs/stages/destroy_env/run_terraform.sh
@@ -13,23 +13,21 @@ terraform -chdir="$PWD/$TERRAFORM_FOLDER_NAME" \
                 -backend-config "dynamodb_table=$DYNAMO_TABLE_TF_LOCK" \
                 > /dev/null
 
-if [ $? -gt 0 ]; then
+if [ $? -ne 0 ]; then
     echo "Error with Terraform Init. Please check again"
     exit 100
 fi
 
 terraform -chdir="$PWD/$TERRAFORM_FOLDER_NAME" \
                 destroy \
-                -auto-approve \
+		-auto-approve \
                 -var "aws_access_key=$AWS_ACCOUNT_CREDENTIALS_USR" \
                 -var "aws_secret_key=$AWS_ACCOUNT_CREDENTIALS_PSW" \
                 -var "aws_region=$AWS_REGION_CHKD" \
-                -var "aws_region_dr=$AWS_REGION_DR_CHKD" \
                 -var "ssh_key=$SSH_KEYPAIR_NAME_CHKD" \
-                -var "ssh_key_dr=$SSH_KEYPAIR_NAME_DR_CHKD" \
                 -var "ascs_instance_type=$ASCS_INSTANCE_TYPE_CHKD" \
                 -var "as_instance_type=$PAS_INSTANCE_TYPE_CHKD" \
-                -var "hana_instance_type=$HANA_INSTANCE_TYPE_CHKD" \
+                -var "oracle_instance_type=$ORACLE_INSTANCE_TYPE_CHKD" \
                 -var "vpc_id=$VPC_ID_CHKD" \
                 -var "subnet_ids=$SUBNET_IDS_CHKD" \
                 -var "dns_zone_name=$PRIVATE_DNS_ZONE_NAME_CHKD" \
@@ -40,12 +38,11 @@ terraform -chdir="$PWD/$TERRAFORM_FOLDER_NAME" \
                 -var "customer_default_sg_id=$SECURITY_GROUP_ID_CHKD" \
                 -var "enable_ha=$ENABLE_HA_CHKD" \
                 -var "ami_id=$AMI_ID_CHKD" \
-                -var "ami_id_dr=$AMI_ID_DR_CHKD" \
+                -var "ami_id_oracle=$AMI_ID_ORACLE_CHKD" \
                 -var "kms_key_arn=$KMS_KEY_ARN" \
-                -var "kms_key_arn_dr=$KMS_KEY_ARN_DR" \
                 -lock-timeout=10m
 
 if [ $? -gt 0 ]; then
-    echo "Error with Terraform Destroy. Please check again"
+    echo "Error with Terraform destroy. Please check again"
     exit 101
 fi
