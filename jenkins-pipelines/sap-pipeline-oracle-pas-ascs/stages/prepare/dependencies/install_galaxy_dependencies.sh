8c7eb1ed678e57ec058d3341cffcb2f1fa934286
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-02-23 05:17:50 -0500

    Update install_galaxy_dependencies.sh

--- jenkins-pipelines/sap-pipeline-oracle-pas-ascs/stages/prepare/dependencies/install_galaxy_dependencies.sh
+++ jenkins-pipelines/sap-pipeline-oracle-pas-ascs/stages/prepare/dependencies/install_galaxy_dependencies.sh
@@ -16,27 +16,27 @@ fi
 # ------------------------------------------------------------------
 # Download ASCS dependencies
 # ------------------------------------------------------------------
-ansible-galaxy install -f -r "ansible-playbooks/aws-sap-ascs-ers/roles/requirements.yml"
+#ansible-galaxy install -f -r "ansible-playbooks/aws-sap-ascs-ers/roles/requirements.yml"
 
-if [ $? -ne 0 ]; then
-    echo "There was an error while downloading ASCS playbook dependencies. Please try again"
-    exit 104
-fi
+#if [ $? -ne 0 ]; then
+#    echo "There was an error while downloading ASCS playbook dependencies. Please try again"
+#    exit 104
+#fi
 # ------------------------------------------------------------------
 # Download Hana dependencies
 # ------------------------------------------------------------------
-ansible-galaxy install -f -r "ansible-playbooks/aws-sap-hana/roles/requirements.yml"
+#ansible-galaxy install -f -r "ansible-playbooks/aws-sap-hana/roles/requirements.yml"
 
-if [ $? -ne 0 ]; then
-    echo "There was an error while downloading Hana playbook dependencies. Please try again"
-    exit 103
-fi
+#if [ $? -ne 0 ]; then
+#    echo "There was an error while downloading Hana playbook dependencies. Please try again"
+#    exit 103
+#fi
 # ------------------------------------------------------------------
 # Download PAS dependencies
 # ------------------------------------------------------------------
-ansible-galaxy install -f -r "ansible-playbooks/aws-sap-pas/roles/requirements.yml"
+#ansible-galaxy install -f -r "ansible-playbooks/aws-sap-pas/roles/requirements.yml"
 
-if [ $? -ne 0 ]; then
-    echo "There was an error while downloading PAS playbook dependencies. Please try again"
-    exit 104
-fi
+#if [ $? -ne 0 ]; then
+#    echo "There was an error while downloading PAS playbook dependencies. Please try again"
+#    exit 104
+#fi
